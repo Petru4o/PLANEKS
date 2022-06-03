12c0801e6c4827e011a19eab1b8b89b0f24b3cc1
Petro <saliypetro22@gmail.com>
2022-06-03 02:16:55 -0700

    Login and Logout system

--- fakedata/urls.py
+++ fakedata/urls.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('', views.MainList.as_view(), name='home'),
+
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- fakedata/views.py
+++ fakedata/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.views.generic import ListView
 
-# Create your views here.
+from fakedata.models import Schema
+
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+
+
+class MainList(ListView):
+    model = Schema
+    template_name = 'home.html'
